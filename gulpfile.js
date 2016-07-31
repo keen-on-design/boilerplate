1a552421fc0da8e23c6e8f652db5f1b2ad54c63a
Nikita <maccuro.media@gmail.com>
2016-08-01 01:46:19 +0300

    добавлен gulp-load-plugins

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+// Include Gulp
+var gulp = require('gulp');
+
+// Include plugins
+var plugins = require("gulp-load-plugins")({
+    pattern: ['gulp-*', 'gulp.*', 'main-bower-files'],
+    replaceString: /\bgulp[\-.]/
+});
+
+// Define default destination folder
+var dest = 'www/public/';
