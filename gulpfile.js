2dff92774326d1a869a172182011f576de7a373d
Nikita <maccuro.media@gmail.com>
2017-01-10 20:50:46 +0300

    testing new Cradle tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
 'use strict';
 var gulp = require('gulp'),
-  util   = require('gulp-util'),
+  util = require('gulp-util'),
   config = require('./gulp/config');
 
 global.$ = {
-  package : require('./package.json'),
+  package: require('./package.json'),
 
   // Environment is stored in env property.
-  env : {
-    production : util.env[require('./gulp/config.env').flags.production] !== undefined,
-    debug      : util.env[require('./gulp/config.env').flags.debug]      !== undefined
+  env: {
+    production: util.env[require('./gulp/config.env').flags.production] !== undefined,
+    debug: util.env[require('./gulp/config.env').flags.debug] !== undefined
   },
 
   /**
@@ -18,16 +18,17 @@ global.$ = {
    * @property path Path to a task file
    * @property config Task config. Optional: default config is set in a task file
    */
-  tasks : [
-    //Generally tasks should be created with config from a global storage: gulp/config.js
-    { id : 'js:bundle', path : './gulp/c.tasks/bundle.js', config : config.bundle },
-    { id : 'js:browserify', path : './gulp/c.tasks/scripts.js', config : config.js },
-    { id : 'fonts', path : './gulp/c.tasks/relocate.js', config : config.fonts },
-    { id : 'sass', path : './gulp/c.tasks/sass.js', config : config.sass },
-    { id : 'js:lint', path : './gulp/c.tasks/eslint.js', config : config.lint },
+  tasks: [
+    //Generally tasks should be created with config from a global storage: gulp/config.js.
+    { id: 'js:lint', path: './gulp/c.tasks/eslint.js', config: config.lint },
+    { id: 'js:bundle', path: './gulp/c.tasks/bundle.js', config: config.bundle },
+    { id: 'js:browserify', path: './gulp/c.tasks/scripts.js', config: config.js },
+    { id: 'sass', path: './gulp/c.tasks/sass.js', config: config.sass },
+    { id: 'fonts', path: './gulp/c.tasks/relocate.js', config: config.fonts },
+    { id: 'images', path: './gulp/c.tasks/images.js', config: config.images },
 
     //In some cases you can create tasks passing inline config to keep things simple and transparent.
-    { id : 'clean:dev', path : './gulp/c.tasks/clean.js', config : {destination: config.destPrd} },
+    { id: 'clean:dev', path: './gulp/c.tasks/clean.js', config: {destination: config.destPrd} },
   ],
 
   browserSync : require('browser-sync').create()
@@ -45,9 +46,10 @@ gulp.task('default', gulp.series(
   'js:lint',
   'clean:dev',
   gulp.parallel(
-    'fonts',
     'js:bundle',
     'js:browserify',
-    'sass'
+    'sass',
+    'fonts',
+    'images'
   )
 ));
